f5841809e51eb051d50472525804bfbd7e779753
Nic01122 <nicofernandez1122@gmail.com>
2025-11-24 16:18:39 -0300

    nuebo archivo posturas

---
+++ posturas.js
@@ -0,0 +1,27 @@
+// --- MANIFIESTO DE POSTURAS ---
+// Este es el único archivo que necesitas editar para añadir o quitar posturas.
+// El nombre que escribas aquí debe ser EXACTAMENTE igual al nombre del archivo en la carpeta FOTOSPOSTURAS,
+// incluyendo mayúsculas, espacios y la extensión .webp.
+
+const LISTA_DE_POSTURAS = [
+    "Sentadilla.webp",
+    "Zancadas.webp",
+    "Puente de Glúteos.webp",
+    "Postura del Triángulo.webp",
+    "Plancha con Manos.webp",
+    "Postura del Sabio.webp",
+    "Superman.webp",
+    "Plancha Lateral.webp",
+    "Plancha Isométrica.webp",
+    "Guerrero 1.webp",
+    "Guerrero 2.webp",
+    "Guerrero 3.webp",
+    "La-Silla.webp",
+    "Angula Lateral Extendido.webp",
+    "Media Torsión Sentada.webp",
+    "La Piramide.webp",
+    "Postura del Águila.webp",
+    "Postura de la Vela.webp",
+    "Savasana Yoga.webp",
+    "Perro-Pajaro.webp"
+];
